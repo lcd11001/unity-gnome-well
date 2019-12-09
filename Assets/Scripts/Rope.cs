8fa9a26a717033b8908e5a2aa8cb5374afa5ac0e
Luong Cong Dan <lcd11001@gmail.com>
2019-12-09 17:15:30 +0700

    + Rope material

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -26,7 +26,7 @@ public class Rope : MonoBehaviour
 
     public void ResetLength()
     {
-        foreach(GameObject segment in ropeSegments)
+        foreach (GameObject segment in ropeSegments)
         {
             Destroy(segment);
         }
@@ -96,6 +96,46 @@ public class Rope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject topSegment = ropeSegments[0];
+        SpringJoint2D topSegmentJoint = topSegment.GetComponent<SpringJoint2D>();
 
+        if (isIncreasing)
+        {
+            if (topSegmentJoint.distance >= maxRopeSegmentLength)
+            {
+                CreateRopeSegment();
+            }
+            else
+            {
+                topSegmentJoint.distance += ropeSpeed * Time.deltaTime;
+            }
+        }
+
+        if (isDecreasing)
+        {
+            if (topSegmentJoint.distance <= 0.005f)
+            {
+                RemoveRopeSegment();
+            }
+            else
+            {
+                topSegmentJoint.distance -= ropeSpeed * Time.deltaTime;
+            }
+        }
+
+        if (lineRenderer != null)
+        {
+            lineRenderer.positionCount = ropeSegments.Count + 2;
+            
+            lineRenderer.SetPosition(0, this.transform.position);
+            
+            for (int i = 0; i < ropeSegments.Count; i++)
+            {
+                lineRenderer.SetPosition(i + 1, ropeSegments[i].transform.position);
+            }
+
+            SpringJoint2D connectedObjectJoint = connectedObject.GetComponent<SpringJoint2D>();
+            lineRenderer.SetPosition(ropeSegments.Count + 1, connectedObject.transform.TransformPoint(connectedObjectJoint.anchor));
+        }
     }
 }
